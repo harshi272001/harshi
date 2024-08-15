f7a28bd0cb58bc348afbf59dc806a97f56cdf78a
PC <PC@Harshitha>
2024-08-15 14:46:31 +0530

    commit

--- src/Programs/Data.java
+++ src/Programs/Data.java
@@ -1,5 +1,19 @@
 package Programs;
 
-public class Data {
-
+public class Data 
+{
+  public static void main(String[] args)
+  {
+	  int []arr= {1,21,30,41};
+	  int sum=0;
+	  for(int i=0; i<arr.length;i++) 
+	  {
+		  if(arr[i]%2==0)
+		  {
+			  sum=sum+arr[i];
+		  }
+		  System.out.println(sum);
+	  }
+  }
 }
+
